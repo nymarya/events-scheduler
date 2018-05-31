eb83f356a8431bcc3c22164bcc14fd5502dd7a67
jainebudke <jainebudke@hotmail.com>
2018-05-31 11:47:05 -0300

    gerar grafo de acordo cm arquivo lido diretamente pelo codigo

--- src/View/Main.java
+++ src/View/Main.java
@@ -1,14 +1,22 @@
 package View;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import Graph.*;
+import Manager.Engine;
 
 public class Main {
 
-	public static void main( String [] args ){
+	public static void main( String [] args ) throws IOException {
+			    
+		Engine engine = new Engine();
+		engine.createGraph();
 		
-		
-		
-		
+
 	}
 	
 }
